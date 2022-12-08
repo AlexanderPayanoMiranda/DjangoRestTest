3c0153594a8f7e9e363e131f3cb2efdd58d446c0
Alexander G.P.M <73145510@continental.edu.pe>
2022-12-08 12:15:34 -0500

    API with class instead of function

--- MyAPI/urls.py
+++ MyAPI/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from MyAPI.views import pais
 
 urlpatterns = [
-    path('pais/', pais, name='pais')
+    # path('pais/', pais, name='pais'),
+    path('pais/', pais.as_view(), name='pais')
 ]
